06fa662deb2caa22c505fe14ee6673a14bd1b19e
NastuzziSamy <samy@nastuzzi.fr>
2019-01-14 18:50:07 +0100

    Improve reflection and work with class maps and declared classes

--- src/ReflectionNamespace.php
+++ src/ReflectionNamespace.php
@@ -16,57 +16,177 @@
 
 class ReflectionNamespace implements \Reflector
 {
-    // Regex back slash
+    // Regex back slash.
     const R_BACKSLASH = '\\\\';
+    const R_NO_BACKSLASH = '((?:(?!'.self::R_BACKSLASH.').)+)';
+    const R_WHATEVER_NAMESPACE = '('.self::R_BACKSLASH.self::R_NO_BACKSLASH.')*';
 
-    const R_NO_BACKSLASH = '((?!'.self::R_BACKSLASH.').)+';
-    const R_SUB_NAMESPACE = '^(.*)'.self::R_BACKSLASH.self::R_NO_BACKSLASH.'$';
+    // Check if loaders changed.
+    protected static $loaders;
+    // Specify if the class should load declared classes.
+    protected static $loadDeclaredClasses = false;
+    // Specify if the class should load PSR0 declarations.
+    protected static $loadPSR0 = false;
 
     protected $name;
     protected $parent;
 
-    protected $ownedClasses;
-    protected $subClasses;
+    // Specific loaders for this instance.
+    protected $customLoaders;
+    protected $hasCustomLoaders = false;
 
-    protected $ownedNamespaces;
-    protected $subNamespaces;
+    // Check if everything is loaded for this instance.
+    protected $prepared = false;
+    protected $classes;
+    protected $namespaces;
 
-    public function __construct($name)
+    /**
+     * Return an instance for the specific namespace.
+     *
+     * @param string $name The name of the namespace.
+     */
+    public function __construct(string $name)
     {
         $this->name = $name;
-        $classes = preg_grep($this->getNamespaceRegex(), self::getLoaderClasses());
-        $this->ownedClasses = array_fill_keys(preg_grep($this->getOwnedRegex(), $classes), null);
-        $subClasses = preg_grep($this->getOwnedRegex(), $classes, PREG_GREP_INVERT);
-        $this->subClasses = array_fill_keys($subClasses, null);
-
-        $namespaces = array_map(function ($class) {
-            preg_match('#'.self::R_SUB_NAMESPACE.'#', $class, $matches);
-
-            return $matches[1];
-        }, $subClasses);
-
-        $this->ownedNamespaces = array_fill_keys(preg_grep($this->getOwnedRegex(), $namespaces), null);
-        $this->subNamespaces = array_fill_keys(preg_grep($this->getOwnedRegex(), $namespaces, PREG_GREP_INVERT), null);
     }
 
-    public static function getLoaders()
+    /**
+     * Tell if the class should load declared classes.
+     *
+     * @param  boolean $load
+     * @return string
+     */
+    public static function loadDeclaredClasses(bool $load = true) {
+        static::$loadDeclaredClasses = $load;
+
+        return static::class;
+    }
+
+    /**
+     * Return if the class should load declared classes.
+     *
+     * @return boolean
+     */
+    public static function isLoadingDeclaredClasses() {
+        return static::$loadDeclaredClasses;
+    }
+
+    /**
+     * Tell if the class should load from PSR0 declarations.
+     *
+     * @param  boolean $load
+     * @return string
+     */
+    public static function loadPRS0($load = true) {
+        static::$loadPSR0 = $load;
+
+        return static::class;
+    }
+
+    /**
+     * Return if the class should load from PSR0 declarations.
+     *
+     * @return boolean
+     */
+    public static function isLoadingPSR0() {
+        return static::$loadPSR0;
+    }
+
+    public static function getLoaders(bool $reload = false)
     {
-        return array_values(array_map(function ($autoloadFunction) {
-            return $autoloadFunction[0];
-        }, array_filter(spl_autoload_functions(), function ($autoloadFunction) {
-            return is_array($autoloadFunction);
+        if (static::$loaders && !$reload) {
+            return static::$loaders;
+        }
+
+        return static::$loaders = array_values(array_map(function ($autoloadClosure) {
+            return $autoloadClosure[0];
+        }, array_filter(spl_autoload_functions(), function ($autoloadClosure) {
+            return is_array($autoloadClosure);
         })));
     }
 
-    public static function getLoaderClasses()
+    public function getLocalLoaders(bool $reload = false)
     {
-        $loaderClasses = [];
-
-        foreach (static::getLoaders() as $loader) {
-            $loaderClasses = array_merge($loaderClasses, array_keys($loader->getClassMap()));
+        if ($this->hasCustomLoaders) {
+            return $this->customLoaders;
         }
 
-        return $loaderClasses;
+        return static::getLoaders($reload);
+    }
+
+    public function setLoaders(array $loaders = [], bool $hasCustomLoaders = true)
+    {
+        $this->customLoaders = $loaders;
+        $this->hasCustomLoaders = $hasCustomLoaders;
+    }
+
+    public function fillWithLoader($loader) {
+        $this->fillFromClassMap(array_keys($loader->getClassMap()));
+    }
+
+    protected function fillFromClassMap(array $classes) {
+        $this->fillClasses($classes);
+        $this->fillNamespaces($classes);
+    }
+
+    protected function fillWithDeclaredClasses()
+    {
+        $this->fillFromClassMap(get_declared_classes());
+        $this->fillFromClassMap(get_declared_interfaces());
+        $this->fillFromClassMap(get_declared_traits());
+    }
+
+    protected function fillClasses(array $classes)
+    {
+        $this->classes = array_merge(
+            array_fill_keys(array_map(function (string $class) {
+                preg_match($this->getClassRegex(), $class, $matches);
+
+                return $matches[1];
+            }, preg_grep($this->getClassRegex(), $classes)), null),
+            $this->classes
+        );
+    }
+
+    protected function fillNamespaces(array $classes)
+    {
+        $this->namespaces = array_merge(
+            array_fill_keys(array_map(function (string $class) {
+                preg_match($this->getNamespaceRegex(), $class, $matches);
+
+                return $matches[1];
+            }, preg_grep($this->getNamespaceRegex(), $classes)), null),
+            $this->namespaces
+        );
+    }
+
+    {
+    }
+
+    }
+
+    }
+
+    }
+
+    protected function prepare()
+    {
+        if (!$this->hasCustomLoaders && static::$loaders !== static::getLoaders(true)) {
+            $this->prepared = false;
+        }
+
+        if (!$this->prepared) {
+            $this->classes = [];
+            $this->namespaces = [];
+
+            foreach ($this->getLocalLoaders() as $loader) {
+                $this->fillWithLoader($loader);
+            }
+
+            if (static::isLoadingDeclaredClasses()) {
+                $this->fillWithDeclaredClasses();
+            }
+        }
     }
 
     public function getName()
@@ -85,7 +205,7 @@ class ReflectionNamespace implements \Reflector
     {
         preg_match($this->getParentNameRegex(), $this->getName(), $matches);
 
-        return $matches[1] ?? '';
+        return $matches[1] ?? null;
     }
 
     public function getParent()
@@ -95,69 +215,51 @@ class ReflectionNamespace implements \Reflector
 
     protected function getNameForRegex()
     {
-        return str_replace('\\', self::R_BACKSLASH, $this->getName());
+        return str_replace('\\', static::R_BACKSLASH, $this->getName());
+    }
+
+    protected function getClassRegex()
+    {
+        return '#^'.$this->getNameForRegex().static::R_BACKSLASH.static::R_NO_BACKSLASH.'$#';
     }
 
     protected function getNamespaceRegex()
     {
-        return '#^'.$this->getNameForRegex().self::R_BACKSLASH.'#';
+        return '#^'.$this->getNameForRegex().static::R_BACKSLASH.static::R_NO_BACKSLASH.static::R_BACKSLASH.static::R_NO_BACKSLASH.'$#';
     }
 
-    protected function getOwnedRegex()
     {
-        return '#^'.$this->getNameForRegex().self::R_BACKSLASH.self::R_NO_BACKSLASH.'$#';
+        return array_intersect($this->getOwnedClassNames(), get_declared_classes());
     }
 
     protected function getShortNameRegex()
     {
-        return '#((?!'.self::R_BACKSLASH.').)+$#';
+        return '#((?!'.static::R_BACKSLASH.').)+$#';
     }
 
     protected function getParentNameRegex()
     {
-        return '#^(.*)'.self::R_BACKSLASH.'#';
+        return '#^(.*)'.static::R_BACKSLASH.'#';
     }
 
     public function getClassNames()
     {
-        return ($this->getOwnedClassNames() + $this->getSubClassNames());
-    }
+        $this->prepare();
 
-    public function getOwnedClassNames()
-    {
-        return array_keys($this->ownedClasses);
-    }
-
-    public function getSubClassNames($direct=true)
-    {
-        return array_keys($this->subClasses);
+        return array_keys($this->classes);
     }
 
     public function getClasses()
     {
-        return array_merge($this->getOwnedClasses(), $this->getSubClasses());
-    }
+        $this->prepare();
 
-    public function getOwnedClasses()
-    {
-        foreach ($this->ownedClasses as $className => $classReflection) {
+        foreach ($this->classes as $className => $classReflection) {
             if (is_null($classReflection)) {
-                $this->ownedClasses[$className] = new \ReflectionClass($className);
+                $this->classes[$className] = new \ReflectionClass($className);
             }
         }
 
-        return $this->ownedClasses;
-    }
-
-    public function getSubClasses()
-    {
-        foreach ($this->subClasses as $className => $classReflection) {
-            if (is_null($classReflection)) {
-                $this->subClasses[$className] = new \ReflectionClass($className);
-            }
-        }
-
-        return $this->subClasses;
+        return $this->classes;
     }
 
     public function hasClass(string $className)
@@ -165,33 +267,11 @@ class ReflectionNamespace implements \Reflector
         return in_array($className, $this->getClassNames());
     }
 
-    public function hasOwnedClass(string $className)
-    {
-        return in_array($className, $this->getOwnedClassNames());
-    }
-
-    public function hasSubClass(string $className)
-    {
-        return in_array($className, $this->getSubClassNames());
-    }
-
     public function getClass(string $className)
     {
-        if ($this->hasOwnedClass($className)) {
-            return $this->getOwnedClass($className);
-        } else {
-            return $this->getSubClass($className);
-        }
-    }
+        $this->prepare();
 
-    public function getOwnedClass(string $className)
-    {
-        return ($this->ownedClasses[$className] ?? ($this->ownedClasses[$className] = new \ReflectionClass($className)));
-    }
-
-    public function getSubClass(string $className)
-    {
-        return ($this->subClasses[$className] ?? ($this->subClasses[$className] = new \ReflectionClass($className)));
+        return ($this->classes[$className] ?? ($this->classes[$className] = new \ReflectionClass($className)));
     }
 
     public function getDeclaredClasses()
@@ -199,53 +279,19 @@ class ReflectionNamespace implements \Reflector
         return array_intersect($this->$this->getClassNames(), get_declared_classes());
     }
 
-    public function getDeclaredOnwedClasses()
-    {
-        return array_intersect($this->getOwnedClassNames(), get_declared_classes());
-    }
-
-    public function getDeclaredSubClasses()
-    {
-        return array_intersect($this->getSubClassNames(), get_declared_classes());
-    }
-
-    public function getOwnedNamespaceNames() {
-        return array_keys($this->ownedNamespaces);
-    }
-
-    public function getSubNamespaceNames() {
-        return array_keys($this->subNamespaces);
-    }
-
     public function getNamespaceNames() {
-        return ($this->getOwnedNamespaceNames() + $this->getSubNamespaceNames());
-    }
-
-    public function getOwnedNamespaces()
-    {
-        foreach ($this->ownedNamespaces as $className => $classReflection) {
-            if (is_null($classReflection)) {
-                $this->ownedNamespaces[$className] = new static($className);
-            }
-        }
-
-        return $this->ownedNamespaces;
-    }
-
-    public function getSubNamespaces()
-    {
-        foreach ($this->subNamespaces as $className => $classReflection) {
-            if (is_null($classReflection)) {
-                $this->subNamespaces[$className] = new static($className);
-            }
-        }
-
-        return $this->subNamespaces;
+        return array_keys($this->namespaces);
     }
 
     public function getNamespaces()
     {
-        return array_merge($this->getOwnedNamespaces(), $this->getSubNamespaces());
+        foreach ($this->namespaces as $className => $classReflection) {
+            if (is_null($classReflection)) {
+                $this->namespaces[$className] = new static($className);
+            }
+        }
+
+        return $this->namespaces;
     }
 
     public static function export()
